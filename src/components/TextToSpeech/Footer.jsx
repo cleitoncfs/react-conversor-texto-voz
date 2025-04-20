4449ad655364c9fe4f6ec20224e7ccdd5180fd24
cleitoncfs <cleitoncfs@gmail.com>
2025-04-20 16:23:51 +0100

    Add link do portfolio no footer

--- src/components/TextToSpeech/Footer.jsx
+++ src/components/TextToSpeech/Footer.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { FaExternalLinkAlt } from "react-icons/fa";
 import "./styles.css";
 
 const Footer = () => {
     return (
         <footer className="footer">
-            <p>
-                © {new Date().getFullYear()} Todos os direitos reservados -
-                Desenvolvido por Cleiton Santos 💻📳
+            <p>© {new Date().getFullYear()} Todos os direitos reservados</p>
+            <p className="footer-credits">
+                Desenvolvido por{" "}
+                <a
+                    href="https://portfolio-cleiton.netlify.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="footer-link"
+                >
+                    <span className="link-text">Cleiton Santos</span>
+                    <FaExternalLinkAlt className="link-icon" />
+                </a>
             </p>
         </footer>
     );
